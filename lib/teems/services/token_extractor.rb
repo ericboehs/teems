9659d1fcf620d3fc45cf428a8e69b1d35e8f3082
Eric Boehs <eric@boehs.com>
2026-03-15 12:41:20 -0500

    Condense wait_for_login to fit Metrics/MethodLength

--- lib/teems/services/token_extractor.rb
+++ lib/teems/services/token_extractor.rb
@@ -329,12 +329,8 @@ module Teems
         consecutive_ready = 0
         60.times do |second|
           sleep 1
-          if page_ready?
-            consecutive_ready += 1
-            break if consecutive_ready >= 3
-          else
-            consecutive_ready = 0
-          end
+          consecutive_ready = page_ready? ? consecutive_ready + 1 : 0
+          break if consecutive_ready >= 3
 
           log("Waiting... (#{second + 1}s)") if ((second + 1) % 10).zero?
         end
